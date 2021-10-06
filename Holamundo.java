3cf653747fc907e42be45a9bf9b09a5ed92ca1bf
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-10-05 20:18:54 -0500

    Hola Mundo en Java
    Subo un Hola Mundo en java después de su primera instalación.

---
+++ Holamundo.java
@@ -0,0 +1,15 @@
+public class Holamundo{
+          /**
+		*Aplicacion java
+		*Tittle      :Hola mundo en java
+		*Description :Hola mundo en java desde vim
+		*Author      :Gustavo Garcia Barrera
+		*Date        :20211005
+		*Version     :1
+		*Usage       :c:\Users\DellDocumentos\holajava
+		*/
+       	public static void main (String [] args){
+		System.out.println("Hola Mundo");//prints Hello World
+	}
+}
+
